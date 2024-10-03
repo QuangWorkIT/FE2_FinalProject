8ba793082609a18fa03842a1a6a4523be61c7cf3
Hoang Tran <pthoangtran0511@gmail.com>
2024-10-03 14:01:27 +0700

    style input

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const apiKey = "498925067a988273c8dc2d9a7a2fd1aa";
-        const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=vietnam";
+        const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=vietnam';
 
         async function checkWeather() {
             const response = await fetch(apiUrl + '&appid=${apiKey}');
             var data = await response.json();
 
             console.log(data);
-
         }
